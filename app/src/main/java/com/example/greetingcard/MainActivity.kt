c683cdda52f403a15071a7cd2e775ec5154d2ba0
Lia Cano <lia.cano@gmail.com>
2024-09-22 21:31:25 -0400

    Se agregaron cambios de diseño

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,18 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greetingcard.ui.theme.GreetingCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Lia",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,7 +43,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface ( color = Color.LightGray ) {
         Text(
             text = "Hi, my name is $name!",
-            modifier = Modifier.padding(25.dp)
+            modifier = Modifier.padding(25.dp).fillMaxSize().wrapContentSize(Alignment.Center),
+            style = TextStyle(fontSize = 40.sp)
         )
     }
 }
